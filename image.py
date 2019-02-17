b3e0dfe331e2427c9d7af2afe6d8286bb8ca45cd
Xairon <ringuetnicolas2@gmail.com>
2019-02-17 16:39:16 +0100

    ajout list frames

--- image.py
+++ image.py
@@ -1,42 +1,95 @@
-import cv2
+# Mon script OpenCV : Video_processing
+# On importe les librairies nécessaires que sont numpy et cv2
 import numpy as np
-
-cap = cv2.VideoCapture("lancelot.mp4")
+import cv2
 kernel = np.ones((5,5),np.uint8)
-for i in range(2650):
-    cap.read()
-while True:
+
+# On définit une fonction de processing de l'image, passant les couleurs en niveaux de gris
+def gray(imgc):
+    return cv2.cvtColor(imgc, cv2.COLOR_BGR2GRAY)
+
+# applique un floue standard à l'image
+def blur(imgc):
+ return cv2.blur(imgc, (5,5))
+
+# applique un floue gaussian à l'image
+def gaussian_blur(imgc):
+ return cv2.GaussianBlur(imgc,(5,5), 0)
+#applique l'algorithme laplacian à l'image
+def laplacian(imgc):
+ return cv2.Laplacian(imgc, cv2.CV_8U)
+
+#applique l'algorithme canny à l'image
+def canny(imgc):
+ return cv2.Canny(imgc, 100, 200)
+#applique le global threshold à l'image
+def global_threshold(imgc):
+ ret, frame = cv2.threshold(imgc, 127, 255, cv2.THRESH_BINARY)
+ return frame
+
+#applique le threshold mean adaptatif à l'image
+def adaptive_mean_threshold(imgc):
+    return cv2.adaptiveThreshold(imgc,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+
+#applique le threshold gaussian adaptatif à l'image
+def adaptive_gaussian_threshold(imgc):
+    return cv2.adaptiveThreshold(imgc,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+#applique l'algorithme de sobel à l'image
+def sobel(imgc):
+    return cv2.Sobel(imgc,cv2.CV_64F,1,0,ksize=5)
+#applique le traitement opening à l'image
+def opening(imgc):
+    return cv2.morphologyEx(adaptive_mean_threshold(imgc), cv2.MORPH_OPEN, kernel)
+#applique le traitement gradient à l'image
+def gradient(imgc):
+    return cv2.morphologyEx(adaptive_mean_threshold(imgc), cv2.MORPH_GRADIENT, kernel)
+    # applique le traitement dilation à l'image
+def dilation(imgc):
+    return cv2.dilate(adaptive_mean_threshold(imgc), kernel, iterations=1)
+# applique le traitement erosion à l'image
+def erosion(imgc):
+    return cv2.erode(adaptive_mean_threshold(imgc), kernel, iterations=1)
+    # applique le traitement closing à l'image
+def closing(imgc):
+    return cv2.morphologyEx(adaptive_mean_threshold(imgc), cv2.MORPH_CLOSE, kernel)
+
+
+
+# On définit une variable associée à l'entrée vidéo que l'on veut traiter
+cap = cv2.VideoCapture('lancelot.mp4')
+
+while (True):
+    # On récupère la prochaine image du flux vidéo ainsi qu'un flag
     ret, frame = cap.read()
-    if ret:
+
+    # Si le flag est à True on traite l'image
+    if ret == True:
+
+        # On copie l'image afin de pouvoir la traiter sans perdre l'orignal
         img = frame.copy()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray,(5,5),8)
-        median = cv2.medianBlur(gray,5)
 
-        sobelx64f = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
-        abs_sobel64f = np.absolute(sobelx64f)
-        sobel_8u = np.uint8(abs_sobel64f)
-        th1 = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
-        th2 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,11,2)   
-        th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
-        opening = cv2.morphologyEx(th2, cv2.MORPH_OPEN, kernel)
-        gradient = cv2.morphologyEx(th2, cv2.MORPH_GRADIENT, kernel)
-        dilation = cv2.dilate(th2, kernel, iterations=1)
-        erosion = cv2.erode(th2, kernel, iterations=1)
-        closing = cv2.morphologyEx(th2, cv2.MORPH_CLOSE, kernel)
-        edges = cv2.Canny(gray,100,200)
-        laplacian = cv2.Laplacian(gray,cv2.CV_8U)
-        sobelx8u = cv2.Sobel(blur, cv2.CV_8U, 1, 0, ksize=5)
+        # On applique ici la fonction gray à la copie
+        img = gray(img)
 
-
-
-        cv2.imshow('MadvideoAvant', frame)
-        cv2.imshow('MavideoApres', closing)
+        # Ici on affiche en sortie deux vidéos, la première étant notre vidéo d'origine et la deuxième celle modifiée
+        cv2.imshow('MavideoAvant', frame)
+        cv2.imshow('MavideoApres', img)
+    # Si le flag est à False, on sort de la boucle
     else:
         print('video ended')
-    if cv2.waitKey(40) & 0xFF == ord('q'):
         break
+
+    # On attend une seconde avant d'afficher la prochaine image et si l'utilisateur presse 'q' on sort de la boucle.
+    if cv2.waitKey(1000) & 0xFF == ord('q'):
+        break
+# When everything's done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
--- scene_detection.py
+++ scene_detection.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import SIFT
+
 # Advanced Scene Detection Parameters
 INTENSITY_THRESHOLD = 16  # Pixel intensity threshold (0-255), default 16
 MINIMUM_PERCENT = 95  # Min. amount of pixels to be below threshold.
@@ -8,11 +8,9 @@ BLOCK_SIZE = 32  # Num. of rows to sum per iteration.
 
 cap = cv2.VideoCapture("lancelot.mp4")
 
-
-
 # Do stuff with cap here.
 
-width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 print(
     "Video Resolution: %d x %d" % (width, height))
@@ -33,7 +31,6 @@ last_amt = 0  # Number of pixel values above threshold in last frame.
 start_time = cv2.getTickCount()  # Used for statistics after loop.
 
 l = []
-t = []
 while True:
     # Get next frame from video.
     (rv, im) = cap.read()
@@ -54,15 +51,13 @@ while True:
         if frame_amt > min_pixels:  # We can avoid checking the rest of the
             break  # frame since we crossed the boundary.
         curr_row += num_rows
-    t.append(im)
     # Detect fade in from black.
     if frame_amt >= min_pixels and last_amt < min_pixels:
         print(
             "Detected fade in at %dms (frame %d)." % (
                 cap.get(cv2.CAP_PROP_POS_MSEC),
                 cap.get(cv2.CAP_PROP_POS_FRAMES)))
-        l.append(t)
-        t= []
+        l.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
 
     # Detect fade out to black.
     elif frame_amt < min_pixels and last_amt >= min_pixels:
@@ -70,12 +65,10 @@ while True:
             "Detected fade out at %dms (frame %d)." % (
                 cap.get(cv2.CAP_PROP_POS_MSEC),
                 cap.get(cv2.CAP_PROP_POS_FRAMES)))
-        l.append(t)
-        t = []
+        l.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
 
     last_amt = frame_amt  # Store current mean to compare in next iteration.
-l.append(t)
-t = []
+l.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
 # Get # of frames in video based on the position of the last frame we read.
 frame_count = cap.get(cv2.CAP_PROP_POS_FRAMES)
 # Compute runtime and average framerate
@@ -87,7 +80,29 @@ print(
         frame_count, total_runtime, avg_framerate))
 
 cap.release()
-listcorrplan = []
-for plan in l:
-    listcorrplan.append(SIFT.representant(plan))
-print(listcorrplan)
+
+cap = cv2.VideoCapture("lancelot.mp4")
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+
+
+
+n = 0
+print(l)
+last = None
+while True:
+    (rv, im) = cap.read()
+    if not rv:
+        break
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == l[n]:
+        if n != 0:
+            cv2.imwrite('test' + str(n) + '.png', last)
+        #last = im
+
+
+        n += 1
+
+        # changement de plan
+    #last = cv2.addWeighted(last, 0.5 + (n / frame_count) * 2, im, 0.5 - (n / frame_count) * 2, 0)
+
